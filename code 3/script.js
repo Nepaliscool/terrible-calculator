563a42ae0f9cfba1877776cdd9f525e41a48f7b8
Nepaliscool <106314128+Nepaliscool@users.noreply.github.com>
2022-10-28 18:48:49 -0400

    Add files via upload

---
+++ code 3/script.js
@@ -0,0 +1,26 @@
+alert(5 + 6);
+var a= 0 ;
+function showValueOfA(){
+  document.getElementById("first number").innerHTML=a;
+  console.log(a);
+}
+showValueOfA();
+
+
+var b="-";
+function showValueOfB(){
+  document.getElementById("operator").innerHTML=b;
+  console.log(b);
+}
+showValueOfB();
+
+document.getElementById("operator").innerHTML = b;
+
+var c= 0 ;
+function showValueOfC(){
+  document.getElementById("second number").innerHTML=c;
+  console.log(c);
+}
+showValueOfC();
+
+document.getElementById("second number").innerHTML = c;
---
+++ code 3/solve.js
@@ -0,0 +1,32 @@
+function solve(){
+    if (b =="+") {
+        if (a > 0) {
+            if (c > 0) {
+                alert(2)
+            } else {
+                alert(1)
+            }
+        } else {
+            if (c > 0) {
+                alert(1)
+            } else {
+                alert(0)
+            }
+        }
+    } else {
+        if (a > 0) {
+            if (c > 0) {
+                alert(0)
+            } else {
+                alert(1)
+            }
+        } else {
+            if (c > 0) {
+                alert(-1)
+            } else {
+                alert(0)
+            }
+        }
+    }
+}
+    
